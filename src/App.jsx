321a42813b7e89252bb45fa8321bf4a650a20c85
Casimia Alex <alexcasimia@gmail.com>
2025-12-28 09:30:06 +0100

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,14 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/*' element={<Layout/>}>
-        <Route index element={<Home />}/> 
-        <Route path='contact' element={<Contact />}/>
-        <Route path='about' element={<About />}/>
-        <Route path='*' element={<Missing />}/>
-      </Route>
-    </Routes>
-      
+      <Routes>
+        <Route path='/*' element={<Layout/>}>
+          <Route index element={<Home />}/> 
+          <Route path='contact' element={<Contact />}/>
+          <Route path='about' element={<About />}/>
+          <Route path='*' element={<Missing />}/>
+        </Route>
+      </Routes>   
   );
 }
 
